a323b870c8813866e0b43ba474dda8309245e1ec
Benjamin Athlan <benjaminathlan@gmail.com>
2011-11-26 19:52:06 +0100

    Added cache control

--- app.rb
+++ app.rb
@@ -12,6 +12,10 @@ set :views, 'views'
 set :public, 'public'
 set :haml, {:format => :html5} # default Haml format is :xhtml
 
+before do
+  response.headers['Cache-Control'] = 'public, max-age=300'
+end
+
 # Application routes
 get '/' do
   haml :index, :layout => :'layouts/application'
