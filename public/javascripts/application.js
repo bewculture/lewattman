a7710435dcb54461a6c345cc1ebf094904cd565f
Benjamin Athlan <benjaminathlan@gmail.com>
2011-12-16 18:50:36 +0100

    Added countdown Code need to be clean

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,38 +7,14 @@ $(document).ready(function() {
 	  .addClass('link external')
 	  .attr('target', '_blank');
 
+
+    	
+    // Initialize a new counter
+     var wattmanCounter = new flipCounter('flip-counter', {value:5184000, inc:-1, pace:1000, auto:true});
+    // 30 Days !!
+
+    if (wattmanCounter.getValue() <= 0){
+        console.log("FINI !");
+    }
+
 });
-
-$(window).load(function()
-  {
-    var scroller = new StickyScroller("#menu",
-        {
-            //start: 42,
-            start: 42,
-            end: 3000,
-            interval: 5,
-            range: 300,
-            margin: 0
-        });
-        
-    var opacity = .25;
-    var fadeTime = 300;
-    var current;
-        
-    scroller.onScroll(function(index)
-          {            
-            // console.log('current index : ' + index);
-              if(scroller.inRange())
-              {
-                if (index >= 54)
-                {
-                  $("#ribbon_one").addClass("fixed");
-                } else if (index < 54)
-                {
-                  $("#ribbon_one").removeClass("fixed");
-                }
-              }
-          });
-  
-
-  });
